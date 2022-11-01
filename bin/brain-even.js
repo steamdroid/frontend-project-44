acf5da2f958db8bfea49b129d98740cc7929100a
steamdroid <steamdroid77@gmail.com>
2022-11-01 23:24:08 +0300

    create brain-even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 import welcomePlayer from '../src/welcomePlayer.js';
+import evenGame from '../src/evenGame.js';
 
 const playerName = welcomePlayer();
+
+evenGame(playerName);
---
+++ src/evenGame.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+import getRandomNumber from './randomNumber.js';
+
+const evenGame = (playerName) => {
+  let isAllAnswersCorrect = true;
+
+  const correctAnswersToWin = 3;
+  let correctAnswers = 0;
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  while (isAllAnswersCorrect && correctAnswers < correctAnswersToWin) {
+    const randomNumber = getRandomNumber(1, 30);
+    const isRandomNumberEven = (randomNumber % 2) === 0;
+    const correctAnswer = isRandomNumberEven ? 'yes' : 'no';
+
+    console.log(`Question: ${randomNumber}`);
+    const playerAnswer = readlineSync.question('Your answer: ');
+
+    if (playerAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswers += 1;
+    } else {
+      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      isAllAnswersCorrect = false;
+    }
+  }
+
+  if (isAllAnswersCorrect) {
+    console.log(`Congratulations, ${playerName}!`);
+  } else {
+    console.log(`Let's try again, ${playerName}!`);
+  }
+};
+
+export default evenGame;
