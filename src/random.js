9ec01237adecc8e399c82db8438d524b17769e27
steamdroid <steamdroid77@gmail.com>
2022-11-02 22:19:20 +0300

    split randomArrayItem func into two

--- src/random.js
+++ src/random.js
@@ -1,7 +1,8 @@
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const getRandomArrayItem = (array) => {
-  const randomIndex = Math.floor(array.length * Math.random());
+export const getRandomArrayIndex = (array) => Math.floor(array.length * Math.random());
 
+export const getRandomArrayItem = (array) => {
+  const randomIndex = getRandomArrayIndex(array);
   return array[randomIndex];
 };
