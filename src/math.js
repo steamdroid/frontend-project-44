f558ae8e8cd4d44b3df8b4d73bfb51548f35b951
steamdroid <steamdroid77@gmail.com>
2022-11-02 21:28:57 +0300

    add greatest common divisor calc function

--- src/math.js
+++ src/math.js
@@ -1,4 +1,4 @@
-const getExpressionResult = (opA, opB, operator) => {
+export const getExpressionResult = (opA, opB, operator) => {
   switch (operator) {
     case '+':
       return opA + opB;
@@ -11,4 +11,15 @@ const getExpressionResult = (opA, opB, operator) => {
   }
 };
 
-export default getExpressionResult;
+export const getGCD = (a, b) => {
+  let numerator = (a > b) ? a : b;
+  let denominator = (a > b) ? b : a;
+
+  while (denominator !== 0) {
+    const nextNumerator = denominator;
+    denominator = numerator % denominator;
+    numerator = nextNumerator;
+  }
+
+  return numerator;
+};
