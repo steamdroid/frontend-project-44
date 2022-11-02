068e3531d6e4674c5d2a008b5314963783ee7b27
steamdroid <steamdroid77@gmail.com>
2022-11-03 00:21:40 +0300

    create isPrimeNumber function

--- src/math.js
+++ src/math.js
@@ -35,3 +35,21 @@ export const getArithmeticProgression = (start, step, length = 10) => {
 
   return progression;
 };
+
+export const isPrimeNumber = (number) => {
+  if (number === 1) {
+    return false;
+  }
+
+  if (number === 2) {
+    return true;
+  }
+
+  for (let i = 3; i < (number - 1); i += 2) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
